9cfa359cd978b590ad5162b000a38327de56094c
owenpercoco <owenpercoco@gmail.com>
2017-06-01 14:21:15 -0400

    saving

---
+++ src/App.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import logo from './logo.svg';
+import './App.css';
+
+class Square extends React.Component {
+
+  render() {
+    return (
+      <button className="square" onClick={() => this.props.onClick()}>
+		<span className="board-value">
+			{this.props.value}
+		</span>
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+   constructor() {
+    super();
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  }
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+		
+			<div className="board-row">
+			  {this.renderSquare(0)}
+			  {this.renderSquare(1)}
+			  {this.renderSquare(2)}
+			</div>
+			<div className="board-row">
+			  {this.renderSquare(3)}
+			  {this.renderSquare(4)}
+			  {this.renderSquare(5)}
+			</div>
+			<div className="board-row">
+			  {this.renderSquare(6)}
+			  {this.renderSquare(7)}
+			  {this.renderSquare(8)}
+			</div>
+		  
+	  </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
+
+
+export default Game;
+
